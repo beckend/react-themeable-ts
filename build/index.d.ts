2e6541e2f567ba0b2561c74fd4c149c4006355cd
Linken Dinh <linken.dinh@gmail.com>
2016-10-29 06:22:10 +0200

    delete build

--- build/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { IClassNameDecoratorFn, IStaticFn, IStaticFnReturn, IStaticFnOptionalReturn, themeable } from './themeable';
-export { IHOCDefaultProps, IThemeDecoratorArgs, TDecComponent, themeDecorator } from './hoc';
